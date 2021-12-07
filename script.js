1e45af4f99f4407c74fedabc678de74c7f4cdcc2
Noah Kim <heanokim@gmail.com>
2021-12-07 10:39:46 -0800

    added buttons for rock, paper, scissors, and logic to play a round whenever one of them is clicked

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// this function makes the computer select a choice for rock, paper, or scissor
 function computerPlay(guess) {
     
     guess = Math.floor(Math.random() * 3 + 1);
@@ -13,7 +14,8 @@ function computerPlay(guess) {
     }
 }
 
-function playRockPaperScissors(playerSelection, computerSelection) {
+// this function gives me the outcome of the game based on my selection and the computer selection
+function playRound(playerSelection, computerSelection) {
 
     if (playerSelection == "rock" && computerSelection == "rock") {
         return "You tied! Rock and rock cancel each other out.";
@@ -36,26 +38,49 @@ function playRockPaperScissors(playerSelection, computerSelection) {
     } else return "Something went wrong.";
 }
 
-function game() {
+// the game function runs the game 5 times
+
+// function game() {
+//     computerSelection = computerPlay();
+//     console.log(playRound(playerSelection, computerSelection));
+//     computerSelection = computerPlay();
+//     console.log(playRound(playerSelection, computerSelection));
+//     computerSelection = computerPlay();
+//     console.log(playRound(playerSelection, computerSelection));
+//     computerSelection = computerPlay();
+//     console.log(playRound(playerSelection, computerSelection));
+//     computerSelection = computerPlay();
+//     console.log(playRound(playerSelection, computerSelection));
+// }
+
+let computerSelection = '';
+let playerSelection = '';
+
+/*  Add an event listener to the buttons that calls the playRound function with
+the correct playerSelection every time a button is clicked. */
+
+let userChoice = document.querySelectorAll('.rps-choice');
+userChoice.forEach(choice => {
+    choice.addEventListener('click', () => {
+        playerSelection = choice.textContent;
+        buttonClick();
+    })
+});
+
+function buttonClick() {
+    playerSelection = playerSelection.toLowerCase();
+    console.log("Me: " + playerSelection);
     computerSelection = computerPlay();
-    console.log(playRockPaperScissors(playerSelection, computerSelection));
-    computerSelection = computerPlay();
-    console.log(playRockPaperScissors(playerSelection, computerSelection));
-    computerSelection = computerPlay();
-    console.log(playRockPaperScissors(playerSelection, computerSelection));
-    computerSelection = computerPlay();
-    console.log(playRockPaperScissors(playerSelection, computerSelection));
-    computerSelection = computerPlay();
-    console.log(playRockPaperScissors(playerSelection, computerSelection));
+    console.log("Computer: " + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
 }
 
-let playerSelection = prompt();
-let computerSelection = computerPlay();
+/* let playerSelection = prompt(); */
 
-console.log(computerSelection);
-playerSelection = playerSelection.toLowerCase();
-console.log(playerSelection);
+// console.log(computerSelection);
+// playerSelection = playerSelection.toLowerCase();
+// console.log(playerSelection);
 
-console.log(playRockPaperScissors(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
 
-game();
+// // game();         
